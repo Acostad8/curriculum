85882225ba2b0af3da4a952e24d9b4216542bd34
Acostad8 <jdacostas@ufpso.edu.co>
2024-05-17 14:46:47 -0500

    load files modified

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,13 @@
-//formas de impresion
-// alert("HOLA DESDE UN ARCHIVO EXTERNO")
-// console.log("HOLA DESDE LA CONSOLA");
-// document.write("HOLA DESDE LA INTERFAZ");
-// Swal.fire({
-//     title: "The Internet?",
-//     text: "That thing is still around?",
-//     icon: "question"
-//   });
-
-
-//VARIABLES y tipos de datos
-const pi = 3.14; //variables que no van a cambiar , decimales
+// VARIABLES Y TIPOS DE DATOS
+const pi = 3.14; //Const: se aplica a valores que no se van a cambiar (DECIMAL) 
 const pul = 2.54;
-//var name = "Juan David Acosta" //se puede usar en todo el codigo ver variables globales , string
-let edad = 18;  //valores que pueden cambiar , enteros
-
-var ver = true; //boolean 
-let fal = false; //boolean 
-
-
-//operadores basicos
-
-// ------valorees enteros y decimales aparecen en morado
-//-------valores caracteres en blanco 
+var name = "Marlon Quintero"; //var: variables globales (STRING)
+let edad = 18;//(ENTERO)
+var ver = true; //(BOOLEAN)
+var fal = false;
 
 
+//OPERADORES BASICOS
 var suma = 5 + 2;
 console.log(suma);
 var var_uno = 10;
@@ -34,86 +17,128 @@ console.log(var_uno * var_dos);
 console.log(var_uno / var_dos);
 console.log(var_uno % var_dos);
 
-//OPERADORES DE COMPÁRACION 
-// =asignacion
-// == comparacion (valor)
-// === estrictamente comparado (compara el valor y el tipo de dato )
-// > / >= / < / <=
+
+//OPERADORES DE COMPARACION
+// = Asignacion
+// == Comparacion (valor)
+// === estrictamente comparado (valor y el tipo de dato )
+// >
+// >=
+// <=
+// <
+// <>
+
+console.log("Mi edad es : " + edad);
 
 
-
-
-console.log("mi edad es: " + edad);
-array = []
-json = []
-
+//EVENTO
 
 function load_page() {
-  Swal.fire(
-    {
-      imageUrl: ("https://ufpso.edu.co/administradoru15/ventana/banner%20Admitidos_Mesa%20de%20trabajo%201%20(3)%20(1).jpg"),
-      showConfirmButton: false,
-      timer: 2000
-    }
-  );
-  document.getElementById("text").innerText = "el array a calcular es " + array_num;
-  document.getElementById("impresion").value = array_num;
-}
+    Swal.fire({
+        imageUrl: "https://ufpso.edu.co/administradoru15/ventana/banner%20Admitidos_Mesa%20de%20trabajo%201%20(3)%20(1).jpg",
+        // imageHeight: 1500,
+        imageAlt: "A tall image",
+        showConfirmButton: false,
+        timer: 2000
+    });
+    document.getElementById("text").innerText = "el array a calcular es " + array_num;
+    document.getElementById("impresion").value = array_num;
+    document.getElementById("name_search").value = nombre_form;
 
+
+  let array_months = ["enero","febrero","marzo","abril","mayo","junio" ,"julio","agosto","septiembre","octubre","noviembre","diciembre"];
+  let day = ["domingo" , "lunes", "martes", "miercoles", "jueves", "viernes", "sabado",] ;
+
+  let hour_system = new Date();
+  let dia = hour_system.getDay();
+
+    
+    console.log(hour_system.getDate());
+    console.log("dia" + hour_system.getDay());
+    console.log(hour_system.getFullYear(array_months));
+    console.log(hour_system.getHours());
+    console.log(hour_system.getMilliseconds());
+    console.log(hour_system.getMinutes());
+    console.log(hour_system.getMonth());
+    console.log(hour_system.getSeconds());
+    console.log(hour_system.getTime());
+
+    document.getElementById("hour_system").value  = hour_system;
+
+
+    console.log("hoy es " + day[dia] );
+    let dia_mes = hour_system.getMonth() +1;
+    console.log(hour_system.getDay()+"/"+hour_system.getMonth()+"/"+hour_system.getFullYear());
+
+
+  
+
+}
 
 function send_form() {
-  let = nombre = document.getElementById("name").value;
-  let = last_name = document.getElementById("last_name").value;
-  console.log(nombre.length);
+    let name = document.getElementById("name").value;
+    let last_name = document.getElementById("last_name").value;
+    let pass_one = document.getElementById("pass_one").value;
+    let pass_two = document.getElementById("pass_two").value;
+    
 
-  if (nombre.length == 0 || last_name.length == 0) {
-    Swal.fire(
-      {
-        title: "cajas de texto vacias",
-        Text: "alguna de las cajas de texto se encuentran vacias",
-        icon: "error"
-      }
-    );
-    if (nombre.length == 0) {
-      document.getElementById("name").styleborder = "2px solid red"
+    if (name.length == 0 || last_name.length == 0|| pass_one.length == 0|| pass_two.length == 0) {
+        Swal.fire({
+            title: "Cajas de texto vacias",
+            text: "Alguna de las cajas de texto estan vacias",
+            icon: "error"
+        });
+
+        // if (name.length == 0) {
+        //     document.getElementById("name").style.border = "2px solid red"
+        // }
+        // else {
+        //     document.getElementById("name").style.border = "2px solid green"
+        // }
+        // if (last_name.length == 0) {
+        //     document.getElementById("last_name").style.border = "2px solid red"
+        // }
+        // else {
+        //     document.getElementById("last_name").style.border = "2px solid green"
+        // }
+
+    }else if(pass_one != pass_two){
+        Swal.fire({
+            title: "Sus contraseñas no son iguales ",
+            text: "Por favor valide sus credenciales",
+            icon: "error"
+        });
     }
     else {
-      document.getElementById("name").styleborder = "2px solid green"
+        document.getElementById("print").innerText = "Su nombre es " + name + "   " + last_name;
+        console.log(isNaN(name));
+
     }
-    if (last_name == 0) {
-      document.getElementById("last_name").styleborder = "2px solid red"
-    }
-    else {
-      document.getElementById("last_name").styleborder = "2px solid green"
-    }
-  }
-  else {
-    document.getElementById("print").innerText = "su nombre es" + name + last_name
-  }
+
 }
 
 
 
-//array
 
+//ARRAYS
 var array_semana = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
-var array_gral = [1, 2, 2.5, "abc", "soy acosta"];
+var array_gral = [1, 2, 3, 4.5, "abc", "Soy Marlon"];
 var array_num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 var array_multi = [
-  { name: "wider", age: 32, color: "verde" },
-  { name: "Marlon", age: 18, color: "rojo" },
-  { name: "acosta", age: 18, color: "azul" },
-  { name: "marian", age: 19, color: "negro" }
+    { name: "Marlon", age: 18, color: "rojo" },
+    { name: "Juan David", age: 18, color: "azul" },
+    { name: "Jefferson", age: 19, color: "verde" },
+    { name: "Antonella", age: 3, color: "blanco" },
+
 ];
 
 var json_ejm = {
-  name: "marlon",
-  lastname: "acosta",
-  phone: "3204886552",
-  email: "jdacosta@ufpso.edu.co"
-
-}
+    name: "Marlon",
+    lastname: "Quintero",
+    phone: "3124834683",
+    email: "mquinterorin@ufpso.edu.co"
+};
 
 console.log(array_semana);
 console.log(array_gral);
@@ -121,84 +146,115 @@ console.log(array_num);
 console.log(array_multi);
 console.log(json_ejm);
 
-
 function action() {
-  // alert(array_num.length);
-  // Swal.fire(array_num);
-  // console.log(array_num);
-  // console.log(array_num.length)
+    //alert(array_num);
+    //Swa.fire(""+array_num);
+    //console.log(array_num);
+    //console.log(array_num.length)
+    var result = 0;
+    for (let i = 0; i < array_num.length; i++) {
+        result = result + array_num[i];
+
+    }
+    document.getElementById("result").innerHTML = "<strong>El resultado es : " + result + "</strong>";
+    document.getElementById("result").style.color = "green";
 
 
-  var result = 0;
+    //CON UN FOR
+    var total = 0;
+    for (var j = 0; j < array_multi.length; j++) {
+        console.log(array_multi[j].age);
+        total = array_multi[j].age + total;
+    }
+    //alert(total);
 
-  for (let i = 0; i < array_num.length; i++) {
-    result = result + array_num[i];
+    //CON UN WHILE
+    var total_dos = 0;
+    var w = 0;
+    while (w < array_multi.length) {
+        total_dos = array_multi[w].age + total_dos;
+        w++;
+    }
+    //alert(total_dos);
 
-  }
-  document.getElementById("result").innerHTML = "<strong> El resultado es: " + result + "</strong>";
-  document.getElementById("result").style.color = "green";
-
-
-
-
-
-
-  var total = 0;
-
-  for (var j = 0; array_multi.length; j++) {
-    console.log(array_multi[j].age);
-    total = array_multi[j].age + total;
-  }
-
-  var total_dos = 0;
-  var w = 0;
-  while (w < array_multi.length) {
-    total_dos = array_multi[w].age + total_dos;
-    w++;
-  }
-  alert(total_dos)
 
 }
 
-
-
-
-
-var array_numerico = [1,2,3,4,5,6,7,8,9];
-function eliminar() {
-  let array_delete = array_numerico.pop();
-  console.log(array_delete);
-  console.log(array_numerico);
-  document.getElementById("impresion").value = array_numerico;
-}
-
+var array_numerico = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function agregar() {
   let num = document.getElementById("num").value;
-  let array_add = array_numerico.push(num);
-  console.log(array_add)
-  console.log(array_numerico)
-  document.getElementById("impresion").value = array_numerico
-}
+  if(isNaN(num) == true){
+    Swal.fire("solo se aceptan numero ");
 
-function reves(){
-  document.getElementById("impresion").value = array_numerico.reverse();
-}
 
-function limpiar(){
-  document.getElementById("num").value = "";
-  document.getElementById("name").value = "";
-  document.getElementById("last_name").value = "";
-  document.getElementById("result").innerText = "";
-}
+  }else{
+    let array_add = array_numerico.push(num);
+    console.log(array_add);
+    console.log(array_numerico);
+    document.getElementById("impresion").value = array_numerico
 
+  }
+    
+}
+function eliminar() {
+    let = array_delete = array_numerico.pop();
+    console.log(array_delete);
+    console.log(array_numerico);
+    document.getElementById("impresion").value = array_numerico
+}
+function reves() {
+    document.getElementById("impresion").value= array_numerico.reverse();
+
+}
 function prim(){
-  let num = document.getElementById("num2").value;
-  let array_add = array_numerico.unshift(num);
-  document.getElementById("impresion").value=array_numerico;
+    let num = document.getElementById("num2").value;
+    let array_add = array_numerico.unshift(num);
+    document.getElementById("impresion").value = array_numerico;
+    
 }
+function limpiar() {
+    document.getElementById("num").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("last_name").value = "";
+    document.getElementById("result").innerText = "";
+}
+
 function eliminarprim(){
-  let array_delete =array_numerico.shift();
-  console.log(array_delete)
-  document.getElementById("impresion").value = array_numerico;
+    let array_delete = array_numerico.shift();
+    document.getElementById("impresion").value=array_numerico;
+}
+
+
+
+var nombre_form = "Wilder Andres Duarte Neira";
+
+function search(){
+    let nombre_buscar = document.getElementById("name_search").value;
+    // alert(nombre_buscar);
+    Swal.fire(nombre_buscar.toLowerCase());
+    // Swal.fire({
+    //     title: nombre_buscar.toLowerCase(),
+    //     text:"Alguna de las cajas de texto se encuentra vacia",
+    //     icon:"error"
+    // })
+
+  
+
+    // Swal.fire(nombre_buscar.charAt(0));
+    
+    // let word =(nombre_buscar.indexOf('e'));
+    // let word = nombre_buscar.lastIndex('e');
+    // let word = nombre_buscar.substring(5,15);
+    let word = nombre_buscar.split("");
+    Swal.fire(word+"");
+
+    let word_com = word.join("");
+    console.log(word_com);
+
+    
+
+    
+
+
 }
